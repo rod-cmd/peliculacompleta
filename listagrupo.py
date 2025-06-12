14d0d8d75a9d88f1b1b977a3ea4073d033becce8
RodrigoMiranda230 <mirandarodrigo246@gmail.com>
2025-06-12 09:16:52 -0400

    Add files via upload

---
+++ listagrupo.py
@@ -0,0 +1,3 @@
+nombres_integrantes = ['Rodrigo Miranda','Fabricio Lora', 'Flavia Gutierrez', 'Joel Escalante', 'Sarai Vidaurre', 'Lizbeth Apaza',]
+for nombre in nombres_integrantes:   
+  print(f"¡Bienvenido a 404 NOT FOUND, {nombre}!")
