265414f453976e16f9f4f36e2449e1d93a156f82
RodrigoMiranda230 <mirandarodrigo246@gmail.com>
2025-06-12 09:17:23 -0400

    Add files via upload

---
+++ listacomidas.py
@@ -0,0 +1,4 @@
+comidasfav = ['pique macho', 'picante', 'sopa de mani',]
+print(f"Mi hobbie favorito es  {comidasfav[0]}")
+print(f"Mi segundo hobbie favorito es  {comidasfav[1]}")
+print(f"Mi tercer hobbie favorito es  {comidasfav[2]}")
