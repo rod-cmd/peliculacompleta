3ea59ecfca31e6c50eba27be1e950396991be15a
RodrigoMiranda230 <mirandarodrigo246@gmail.com>
2025-06-12 09:16:04 -0400

    Add files via upload

---
+++ listamishobbies.py
@@ -0,0 +1,8 @@
+hobbies = ["escuchar musica", "mirar peliculas", "jugar videojuegos"]
+print(f"Mi hobbie favorito es  {hobbies[0]}")
+print(f"Mi segundo hobbie favorito es  {hobbies[1]}")
+print(f"Mi tercer hobbie favorito es  {hobbies[2]}")
+
+
+
+
