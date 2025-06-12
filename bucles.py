975ded156bfe4de811af07e8590121bc19a3920c
RodrigoMiranda230 <mirandarodrigo246@gmail.com>
2025-06-12 09:15:13 -0400

    Add files via upload

---
+++ bucles.py
@@ -0,0 +1,13 @@
+mensaje = input("escribe elmensaje que desear repetir: ")
+veces = int(input("¿cuantas veces deseas repetirlo? "))
+for i in range(veces):
+    print(mensaje)
+
+mensaje = input("escribe elmensaje que desear repetir: ")
+veces = int(input("¿cuantas veces deseas repetirlo? "))
+contador = 0
+while contador < veces:
+    print(mensaje)
+    contador += 1
+
+    
