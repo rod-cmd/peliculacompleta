5eb9ea21ce14dfb2cd1578819356a7c2d69063d4
RodrigoMiranda230 <mirandarodrigo246@gmail.com>
2025-06-17 09:10:10 -0400

    Add files via upload

---
+++ lista2.py
@@ -0,0 +1,5 @@
+
+lista_invertida = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5]
+print ("Lista original:", lista_invertida)
+lista_invertida.reverse()
+print("Lista invertida:", lista_invertida) 
